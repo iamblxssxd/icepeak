7e74a77facaefe45b36e6011c4f4d3c5a0b19c86
blxssxd <iamblxssxd@gmail.com>
2021-08-05 10:38:24 -0400

    Add header components and styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,9 @@ import { useStaticQuery, graphql } from "gatsby"
 import { createGlobalStyle, ThemeProvider } from "styled-components"
 import { normalize } from "styled-normalize"
 
+// components
+import Header from "./header"
+
 const GlobalStyle = createGlobalStyle`
   ${normalize}
   * {
@@ -50,6 +53,7 @@ const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={lightTheme}>
       <GlobalStyle />
+      <Header />
       <main>{children}</main>
     </ThemeProvider>
   )
