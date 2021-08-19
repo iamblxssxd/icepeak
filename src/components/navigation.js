44b450ffe00118673fee801e66a267d43f6109de
blxssxd <iamblxssxd@gmail.com>
2021-08-19 18:37:10 -0400

    Create dynamically rendered projects in the nav menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,6 +11,27 @@ import {
   NavFooter,
 } from "../styles/navigationStyles"
 
+const navRoutes = [
+  {
+    id: 0,
+    title: "death no more",
+    path: "/death-no-more",
+    video: "death-no-more.mp4",
+  },
+  {
+    id: 1,
+    title: "marching",
+    path: "/marching",
+    video: "marching.mp4",
+  },
+  {
+    id: 2,
+    title: "boo-hoo",
+    path: "/boo-hoo",
+    video: "boo-hoo.mp4",
+  },
+]
+
 const Navigation = () => {
   return (
     <Nav>
@@ -28,25 +49,27 @@ const Navigation = () => {
         </NavHeader>
         <NavList>
           <ul>
-            <li>
-              <Link to="/projects/project-name">
-                <div className="link">
-                  <span className="arrow">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 101 57"
-                    >
-                      <path
-                        d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
-                        fill="#FFF"
-                        fillRule="evenodd"
-                      ></path>
-                    </svg>
-                  </span>
-                  Death No More
-                </div>
-              </Link>
-            </li>
+            {navRoutes.map(route => (
+              <li key={route.id}>
+                <Link to={`/projects/${route.path}`}>
+                  <div className="link">
+                    <span className="arrow">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 101 57"
+                      >
+                        <path
+                          d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
+                          fill="#FFF"
+                          fillRule="evenodd"
+                        ></path>
+                      </svg>
+                    </span>
+                    {route.title}
+                  </div>
+                </Link>
+              </li>
+            ))}
           </ul>
         </NavList>
         <NavFooter></NavFooter>
